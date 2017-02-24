0c24c9e124bae60eb3d71c08a646de6f95f6f9fe
Ted Stein <teds@microsoft.com>
2017-02-24 14:11:58 -0800

    wip: Java builds.

--- compiler/src/Language/Bond/Codegen/Templates.hs
+++ compiler/src/Language/Bond/Codegen/Templates.hs
@@ -51,6 +51,9 @@ module Language.Bond.Codegen.Templates
     , comm_interface_cs
     , comm_proxy_cs
     , comm_service_cs
+      -- ** Java
+    , JavaFieldMapping(..)
+    , class_java
     )
     where
 
@@ -65,6 +68,7 @@ import Language.Bond.Codegen.Cpp.Comm_cpp
 import Language.Bond.Codegen.Cpp.Comm_h
 import Language.Bond.Codegen.Cs.Types_cs
 import Language.Bond.Codegen.Cs.Comm_cs
+import Language.Bond.Codegen.Java.Class_java
 -- redundant imports for haddock
 import Language.Bond.Codegen.TypeMapping
 import Language.Bond.Syntax.Types
